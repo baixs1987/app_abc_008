b8df8ef27e6ec14f9ea64533bce35291563ea2d7
asd <546380172@qq.com>
2018-07-06 21:52:42 +0800

    更新二次项目

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -1,7 +1,9 @@
 import allure
+import pytest
 
 class Test_001:
-
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    @allure.step(titil="fd")
     def test_001_1(self):
         allure.attach("描述","输入用户名")
         allure.attach("描述", '输入密码')
